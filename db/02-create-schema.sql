06219e39029d016e4cf519ceb7a3820506164fa4
Teresa Wu <teresa.wu.jy@gmail.com>
2022-08-23 16:13:42 -0700

    Cleaned up pseudocode

--- db/02-create-schema.sql
+++ db/02-create-schema.sql
@@ -1,35 +1,5 @@
 \connect sauce_db
 
----
---- Create the tables in your database
----
--- CREATE TABLE categories (
---     id SERIAL NOT NULL PRIMARY KEY,
---     title VARCHAR(250) NOT NULL UNIQUE,
---     canon BOOLEAN DEFAULT true
--- );
-
--- CREATE TABLE games (
---     id SERIAL NOT NULL PRIMARY KEY,
---     episode_id INTEGER NOT NULL,
---     aired VARCHAR(10),
---     canon BOOLEAN DEFAULT true
--- );
-
--- CREATE TABLE clues (
---     id SERIAL NOT NULL PRIMARY KEY,
---     row_index INTEGER NOT NULL,
---     column_index INTEGER NOT NULL,
---     answer TEXT NOT NULL,
---     question TEXT NOT NULL,
---     game_id INTEGER REFERENCES games(id) ON DELETE CASCADE,
---     board_index INTEGER NOT NULL,
---     value INTEGER NOT NULL,
---     category_id INTEGER NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
---     invalid_count INTEGER DEFAULT 0,
---     canon BOOLEAN DEFAULT true
--- );
-
 CREATE TABLE locations (
     id SERIAL NOT NULL PRIMARY KEY,
     address TEXT NOT NULL,
