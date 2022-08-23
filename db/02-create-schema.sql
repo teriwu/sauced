1801f23e91ba0eada411df1c375544887a6799bc
Jacob <Jacobkim1996@yahoo.com>
2022-08-23 16:34:53 -0700

    Got rid of the trailing commas

--- db/02-create-schema.sql
+++ db/02-create-schema.sql
@@ -6,12 +6,12 @@ CREATE TABLE locations (
     city TEXT NOT NULL,
     zip_code INTEGER NOT NULL,
     country TEXT NOT NULL,
-    state TEXT NOT NULL,
+    state TEXT NOT NULL
 );
 
 CREATE TABLE pictures (
     id SERIAL NOT NULL PRIMARY KEY,
-    url VARCHAR DEFAULT "", 
+    url VARCHAR DEFAULT ""
 );
 
 CREATE TABLE hours (
@@ -19,13 +19,13 @@ CREATE TABLE hours (
     is_overnight BOOLEAN DEFAULT false,
     end INTEGER NOT NULL,
     day INTEGER NOT NULL,
-    start INTEGER NOT NULL,
+    start INTEGER NOT NULL
 );
 
 CREATE TABLE categories (
     id SERIAL NOT NULL PRIMARY KEY,
     title TEXT NOT NULL,
-    alias TEXT NOT NULL,
+    alias TEXT NOT NULL
 );
 
 CREATE TABLE resaurants (
@@ -38,5 +38,5 @@ CREATE TABLE resaurants (
     location_id TEXT NOT NULL REFERENCES locations(id) ON DELETE CASCADE,
     category_id TEXT NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
     hour_id TEXT NOT NULL REFERENCES hours(id) ON DELETE CASCADE,
-    picture_id TEXT NOT NULL REFERENCES pictures(id) ON DELETE CASCADE,
+    picture_id TEXT NOT NULL REFERENCES pictures(id) ON DELETE CASCADE
 );
