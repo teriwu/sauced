24011da2b891302ff3d4f84c4b9128403e966a63
Jacob <Jacobkim1996@yahoo.com>
2022-08-24 15:18:07 -0700

    updated reviews and users.py

--- api/models/reviews.py
+++ api/models/reviews.py
@@ -2,16 +2,6 @@ from pydantic import BaseModel
 from .users import UserOut
 from .restaurants import RestaurantOut
 
-
-class ReviewOut(BaseModel):
-    id: int
-    title: str
-    content: str
-    reviewer_id: list[UserOut]
-    rating: float
-    restaurant_id: list[RestaurantOut]
-
-
 # class ReviewIn(BaseModel):
 #     title: str
 #     content: str
@@ -21,3 +11,12 @@ class ReviewOut(BaseModel):
 
 
 # class ReviewList(BaseModel)
+
+class ReviewOut(BaseModel):
+    id: int
+    title: str
+    content: str
+    reviewer_id: list[UserOut]
+    rating: float
+    restaurant_id: list[RestaurantOut]
+
