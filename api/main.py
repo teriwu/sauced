ada4b2ca428bb101f41b27a45126013b7461e6dc
benny.huang <bhuang997@gmail.com>
2022-08-30 17:46:55 -0700

    Fixed typo error

--- api/main.py
+++ api/main.py
@@ -4,11 +4,8 @@ import os
 
 from routers import (
     restaurants as psql_restaurants,
-<<<<<<< HEAD
     categories as psql_categories,
-=======
     locations as psql_locations,
->>>>>>> 17555340db2adf5bcc9af04f63e97c318a7910eb
 )
 
 app = FastAPI()
@@ -28,8 +25,5 @@ app.add_middleware(
 
 # PostgreSQL endpoints
 app.include_router(psql_restaurants.router)
-<<<<<<< HEAD
 app.include_router(psql_categories.router)
-=======
 app.include_router(psql_locations.router)
->>>>>>> 17555340db2adf5bcc9af04f63e97c318a7910eb
