8a19f91419ca2aa2e56576c445d5b19ec427f59a
Teresa Wu <teresa.wu.jy@gmail.com>
2022-08-31 16:09:21 -0700

    Initial React page(s)

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './MainPage';
+import Nav from './Nav';
+import RestaurantForm from './RestaurantForm';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="restaurants/new" element={<RestaurantForm />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ ghi/src/MainPage.js
@@ -0,0 +1,16 @@
+/* Taken from conference-go */
+function MainPage() {
+    return (
+      <div className="px-4 py-5 my-5 text-center">
+        <h1 className="display-5 fw-bold">LOST IN THE SAUCE</h1>
+        <div className="col-lg-6 mx-auto">
+          <p className="lead mb-4">
+            Find reviews of restaurants or review them yourself!
+          </p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default MainPage;
+  
