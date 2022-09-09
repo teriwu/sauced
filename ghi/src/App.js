cc372bf4bdab62841e8a61241af613ef31618348
Teresa Wu <teresa.wu.jy@gmail.com>
2022-09-09 16:44:10 -0700

    Created states, fetching from restaurants api

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,13 +1,31 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { MainContext } from './MainContext';
 import MainPage from './MainPage';
 import Nav from './Nav';
 import RestaurantForm from './RestaurantForm';
 import RestaurantList from './RestaurantLists';
 
 
-
 function App() {
+
+  const [matchingResults, setMatchingResults] = useState([]);
+  const [dataArr, setDataArr] = useState([]);
+  
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/restaurants")
+    .then(res => res.json())
+    .then(data => {
+      console.log("DATA:", data)
+      setDataArr(data.restaurants);
+    })
+  }, [])
+
   return (
+    <MainContext.Provider value ={{
+      matchingResults, setMatchingResults,
+      dataArr, setDataArr
+    }}>
     <BrowserRouter>
       <Nav />
       <div className="container">
@@ -18,6 +36,7 @@ function App() {
         </Routes>
       </div>
     </BrowserRouter>
+    </MainContext.Provider>
   );
 }
 
