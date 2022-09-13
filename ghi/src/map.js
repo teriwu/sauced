ed1f7fc78e5201bd451ede47ca2b82e750afac51
benny.huang <bhuang997@gmail.com>
2022-09-13 09:23:26 -0700

    Got markers & input box filled

--- ghi/src/map.js
+++ ghi/src/map.js
@@ -1,17 +1,22 @@
 import React from "react";
 import {
-    Box,
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  HStack,
+  Icon,
+  IconButton,
+  Input,
+  Text,
   } from '@chakra-ui/react'
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, DirectionsRenderer, Marker, Autocomplete } from "@react-google-maps/api";
+import { FaLocationArrow, FaTimes } from 'react-icons/fa'
+import { useState, useRef } from "react";
 
-const containerStyle = {
-    width: '',
-    height: '800px',
-};
-  
 const center = {
-    lat: -3.745,
-    lng: -38.523,
+    lat: 37.8185,
+    lng: -122.4738,
 };
 
 function Map() {
@@ -20,37 +25,74 @@ function Map() {
         libraries: ['places'],
     })
 
-    const [map, setMap] = React.useState(null)
+    const [map, setMap] = React.useState( /** @type google.maps.Map */ (null))
 
-    const onLoad = React.useCallback(function callback(map) {
-        const bounds = new window.google.maps.LatLngBounds(center);
-        map.fitBounds(bounds);
-        setMap(map)
-    }, [])
-    
-    const onUnmount = React.useCallback(function callback(map) {
-        setMap(null)
-    }, [])
+
 
     return isLoaded ? (
+      <Flex
+        position='relative'
+        flexDirection={'column'}
+        alignItems='center'
+        h={'800px'}
+        w={'100%'}>
         <Box position='absolute' left={0} top={0} h='100%' w='100%'>
         {/* Google Map Box */}
-        <GoogleMap
-          center={center}
-          zoom={15}
-          mapContainerStyle={containerStyle}
-          options={{
-            zoomControl: false,
-            streetViewControl: false,
-            mapTypeControl: false,
-            fullscreenControl: false,
-          }}
-          onLoad={onLoad}
-          onUnmount={onUnmount}
+          <GoogleMap
+            center={center}
+            zoom={10}
+            mapContainerStyle={{ width: '100%', height: '100%' }}
+            options={{
+              mapTypeControl: false,
+              fullscreenControl: false,
+            }}
+            onLoad={map => setMap(map)}
+          >
+            <Marker position={center} />
+            
+          </GoogleMap>
+        </Box>
+        <Box
+          p={3}
+          borderRadius='lg'
+          m={3}
+          bgColor='white'
+          shadow='base'
+          minW='container.sm'
+          zIndex='1'
         >
-        </GoogleMap>
-      </Box>
-    ) : <></>
+          <HStack>
+            <Box flexGrow={1}>
+              <Autocomplete>
+                <Input type='text' placeholder='Start' />
+              </Autocomplete>
+            </Box>
+            <Box flexGrow={1}>
+              <Autocomplete>
+                <Input type='text' placeholder='End' />
+              </Autocomplete>
+            </Box>
+            <ButtonGroup>
+              <Button bgColor={'skyblue'} type="submit">
+                Get directions
+              </Button>
+              <Button bgColor={'red.500'} type="submit">
+                Cancel
+              </Button>
+            </ButtonGroup>
+          </HStack>
+          <HStack spacing={3} mt={3} justifyContent='space-between'>
+            <text>Distance: </text>
+            <text>Duration: </text>
+            <IconButton 
+            icon={<FaLocationArrow />} 
+            onClick={() => {map.panTo(center); map.setZoom(10)}}
+            />
+          </HStack>
+        </Box>
+      </Flex>
+    )
+    : <></>
 }
 
 export default Map
