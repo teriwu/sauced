90e00333ee3f7ecee76e57e91c423e5581271d58
Lakhvinder Dhanoya <dhanoyal@live.com>
2022-09-13 10:40:00 -0700

    CI work 15

--- api/routers/reviews.py
+++ api/routers/reviews.py
@@ -1,19 +1,16 @@
 from fastapi import APIRouter, Depends, Response, status
 from db import ReviewQueries
-from models.reviews import(
-    ReviewIn,
-    ReviewOut,
-    ReviewList,
-    ReviewDeleteOperation
-)
+from models.reviews import ReviewIn, ReviewOut, ReviewList, ReviewDeleteOperation
 
 router = APIRouter()
+
 
 @router.get("/api/reviews", response_model=ReviewList, tags=["Reviews"])
 def reviews_list(queries: ReviewQueries = Depends()):
     return {
         "reviews": queries.get_reviews(),
     }
+
 
 @router.get("/api/reviews/{id}", response_model=ReviewOut, tags=["Reviews"])
 def get_review(id: int, response: Response, queries: ReviewQueries = Depends()):
@@ -23,12 +20,13 @@ def get_review(id: int, response: Response, queries: ReviewQueries = Depends()):
     else:
         return record
 
+
 @router.post("/api/reviews", response_model=ReviewOut, tags=["Reviews"])
 def create_review(review: ReviewIn, queries: ReviewQueries = Depends()):
     return queries.create_review(review)
 
 
-@router.put("/api/reviews/{id}", response_model = ReviewOut, tags =["Reviews"])
+@router.put("/api/reviews/{id}", response_model=ReviewOut, tags=["Reviews"])
 def update_review(
     review_id: int,
     review_in: ReviewIn,
@@ -42,7 +40,9 @@ def update_review(
         return record
 
 
-@router.delete("/api/reviews/{id}", response_model=ReviewDeleteOperation, tags=["Reviews"])
+@router.delete(
+    "/api/reviews/{id}", response_model=ReviewDeleteOperation, tags=["Reviews"]
+)
 def delete_review(review_id: int, queries: ReviewQueries = Depends()):
     try:
         queries.delete_review(review_id)
