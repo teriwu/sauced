ac53f46a83bcea53cc15b5513e7ec732ae0d1f76
Lakhvinder Dhanoya <dhanoyal@live.com>
2022-09-13 11:28:06 -0700

    CI work 26

--- api/routers/reviews.py
+++ api/routers/reviews.py
@@ -9,24 +9,18 @@ from models.reviews import (
 router = APIRouter()
 
 
-@router.get(
-    "/api/reviews",
-    response_model=ReviewList,
-    tags=["Reviews"])
+@router.get("/api/reviews", response_model=ReviewList, tags=["Reviews"])
 def reviews_list(queries: ReviewQueries = Depends()):
     return {
         "reviews": queries.get_reviews(),
     }
 
 
-@router.get(
-    "/api/reviews/{id}",
-    response_model=ReviewOut,
-    tags=["Reviews"])
+@router.get("/api/reviews/{id}", response_model=ReviewOut, tags=["Reviews"])
 def get_review(
     id: int,
-    response: Response,
-    queries: ReviewQueries = Depends()):
+    response: Response, queries: ReviewQueries = Depends()
+):
     record = queries.get_review(id)
     if record is None:
         response.status_code = 404
@@ -34,23 +28,18 @@ def get_review(
         return record
 
 
-@router.post(
-    "/api/reviews",
-    response_model=ReviewOut,
-    tags=["Reviews"])
+@router.post("/api/reviews", response_model=ReviewOut, tags=["Reviews"])
 def create_review(review: ReviewIn, queries: ReviewQueries = Depends()):
     return queries.create_review(review)
 
 
-@router.put(
-    "/api/reviews/{id}",
-    response_model=ReviewOut,
-    tags=["Reviews"])
+@router.put("/api/reviews/{id}", response_model=ReviewOut, tags=["Reviews"])
 def update_review(
     review_id: int,
     review_in: ReviewIn,
     response: Response,
-    queries: ReviewQueries = Depends(),):
+    queries: ReviewQueries = Depends(),
+):
     record = queries.update_review(review_id, review_in)
     if record is None:
         response.status_code = 404
@@ -59,12 +48,11 @@ def update_review(
 
 
 @router.delete(
-    "/api/reviews/{id}",
-    response_model=ReviewDeleteOperation,
-    tags=["Reviews"])
+    "/api/reviews/{id}", response_model=ReviewDeleteOperation, tags=["Reviews"]
+)
 def delete_review(review_id: int, queries: ReviewQueries = Depends()):
     try:
         queries.delete_review(review_id)
         return {"result": True}
-    except:
+    except ValueError:
         return {"result": False}
