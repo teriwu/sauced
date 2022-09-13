bc4d98b7082c55e049048d63f7cda9555b3a3ea7
Lakhvinder Dhanoya <dhanoyal@live.com>
2022-09-13 10:31:21 -0700

    CI work 12

--- api/db.py
+++ api/db.py
@@ -879,7 +879,8 @@ class CategoriesQueries:
 #                     WHERE id = %s
 #                     RETURNING id, is_overnight, end_, day, start_
 #                     """,
-#                     [data.is_overnight, data.end_, data.day, data.start_, hour],
+#                     [data.is_overnight, data.end_
+# , data.day, data.start_, hour],
 #                 )
 
 #                 record = None
