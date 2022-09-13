2c8b0d8f5bdd88e84cd3a9804a56b22e0798a7d3
Teresa Wu <teresa.wu.jy@gmail.com>
2022-09-13 16:29:12 -0700

    Initial import

---
+++ ghi/src/RestaurantDetail.js
@@ -0,0 +1,9 @@
+import React, {useContext} from "react";
+import { MainContext } from "./MainContext"; 
+import { Link } from 'react-router-dom';
+
+function RestaurantDetail() {
+
+}
+
+export default RestaurantDetail;
