44d29ed872c0244789ee6f13c4826546e1460004
Jacob <Jacobkim1996@yahoo.com>
2022-09-14 12:29:30 -0700

    DETAIL is working  again

--- ghi/src/RestaurantLists.js
+++ ghi/src/RestaurantLists.js
@@ -3,8 +3,8 @@ import { MainContext } from './MainContext';
 import { Link } from 'react-router-dom';
 
 
-function RestaurantList({setRestaurant}) {
-    const { matchingResults, dataArr } = useContext(MainContext);
+function RestaurantList() {
+    const { matchingResults, dataArr, setRestaurant } = useContext(MainContext);
     const [pageLoaded, setPageLoaded] = useState(false);
     const handleRefresh = () => {
         window.location.reload(); 
@@ -26,18 +26,40 @@ function RestaurantList({setRestaurant}) {
                         <th>Location</th>
                         <th>Price</th>
                         <th>Rating</th>
+                        <th></th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
-                    {this.state.restaurants.map(restaurant => {
+                    {matchingResults.length !==0 ? matchingResults.map(restaurant => {
                         return (
-                            <tr key={restaurant.id}>
-                                <td><img src={restaurant.picture} height="150" width="150"/></td>
-                                <td>{ restaurant.name }</td>
+                            <tr onClick={() => {setRestaurant(restaurant)}} key={restaurant.id}>
+                                <td><img src={restaurant.picture}  width="150"/></td>
+                                <td>
+                                <Link to="/restaurants/detail">
+                                    { restaurant.name }
+                                </Link>
+                                </td>      
                                 <td>{ restaurant.city }</td>
                                 <td>{ restaurant.price }</td>
                                 <td>{ restaurant.rating }</td>
-                                <td><Link to="/map" className="btn btn-info btn-sm px-4 gap-3">Get directions</Link></td>
+                                <td><Link to="" className="btn btn-info btn-sm px-4 gap-3">Get directions</Link></td>
+                                <td><Link to="/reviews/new" className="btn btn-info btn-sm px-4 gap-3">Write a Review</Link></td>
+                            </tr>
+                        )
+                    }) : dataArr.map(restaurant => {
+                        return (
+                            <tr onClick={() => {setRestaurant(restaurant)}} key={restaurant.id}>
+                                <td><img src={restaurant.picture}  width="150"/></td>
+                                <td>
+                                <Link to="/restaurants/detail">
+                                    { restaurant.name }
+                                </Link>
+                                </td>    
+                                <td>{ restaurant.city }</td>
+                                <td>{ restaurant.price }</td>
+                                <td>{ restaurant.rating }</td>
+                                <td><Link to="" className="btn btn-info btn-sm px-4 gap-3">Get directions</Link></td>
                                 <td><Link to="/reviews/new" className="btn btn-info btn-sm px-4 gap-3">Write a Review</Link></td>
                             </tr>
                         )
@@ -49,4 +71,5 @@ function RestaurantList({setRestaurant}) {
         </>
     )
 }
+
 export default RestaurantList
