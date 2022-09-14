cc72d5c4db7a0f8d699682133072e60cb7bbb304
Teresa Wu <teresa.wu.jy@gmail.com>
2022-09-14 11:58:46 -0700

    Fixing node_module

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -4,7 +4,7 @@ function MainPage () {
     <>
       <div className='container'>
         <div className="px-4 py-5 my-5 text-center">
-          <img className="bg-white rounded shadow d-block mx-auto mb-4" src="https://alitools.io/en/showcase/image?url=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHTB11h0Lae3tHKVjSZSgq6x4QFXaK%2Fvanzlife-Plastic-squeeze-sauce-bottle-home-ketchup-salad-squeeze-bottle-seasoning-bottle-sauce-honey-jam-juice.jpg" width="600"/>
+          <img className="bg-white rounded shadow d-block mx-auto mb-4" src="https://alitools.io/en/showcase/image?url=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHTB11h0Lae3tHKVjSZSgq6x4QFXaK%2Fvanzlife-Plastic-squeeze-sauce-bottle-home-ketchup-salad-squeeze-bottle-seasoning-bottle-sauce-honey-jam-juice.jpg" alt="sauce" width="600"/>
           <h1 className="display-5 fw-bold">LOST IN THE SAUCE</h1>
           <div className="col-lg-6 mx-auto">
             <p className="lead mb-4">
