77bb96aa7ce5d281e0531cb8cbe3841dcc672130
Lakhvinder Dhanoya <dhanoyal@live.com>
2022-09-15 16:12:42 -0700

    picture test

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -4,7 +4,8 @@ function MainPage () {
     <>
       <div className='container'>
         <div className="sauce-container sauce-font text-white px-4 py-5 text-center">
-          <img className="bg-white rounded shadow d-block mx-auto" src="../sauce.png" alt="sauce" width="600"/>
+          <img className="bg-white rounded shadow d-block mx-auto" src="https://cdn-icons-png.flaticon.com/512/3082/3082037.png
+" alt="sauce" width="600"/>
           <h1 className="centered display-5 fw-bold">LOST IN THE SAUCE</h1>
         </div>
         <div className="col-lg-6 mx-auto">
