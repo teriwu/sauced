cb70a85df62f82e3c246142257604beb13714c4f
Lakhvinder Dhanoya <dhanoyal@live.com>
2022-09-16 09:36:13 -0700

    tests

--- api/tests/routers/test_restaurant.py
+++ api/tests/routers/test_restaurant.py
@@ -6,9 +6,13 @@ from db import RestaurantQueries
 client = TestClient(app)
 
 
+# Lakhvinder's test
 class EmptyRestaurantQueries:
     def get_restaurant(self, id):
         return None
+
+
+# Lakhvinder's test
 
 
 class NormalRestaurantQueries:
@@ -20,17 +24,37 @@ class NormalRestaurantQueries:
 def test_get_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Lost":"sauce"}
+    assert response.json() == {"Lost": "sauce"}
 
 
 # Works with the default dummy data
 def test_get_restaurant():
-    response = client.get("/api/restaurants/1", headers={"Content-Type": "application/json"})
+    response = client.get(
+        "/api/restaurants/1", headers={"Content-Type": "application/json"}
+    )
     assert response.status_code == 200
-    assert response.json() == {"id":1,"price":"string","rating":0.0,"name":"string","phone":"string","address":"string","city":"string","zip_code":0,"country":"string","state":"string","start_":"string","end_":"string","day":"string","picture":"string"}
+    assert response.json() == {
+        "id": 1,
+        "price": "string",
+        "rating": 0.0,
+        "name": "string",
+        "phone": "string",
+        "address": "string",
+        "city": "string",
+        "zip_code": 0,
+        "country": "string",
+        "state": "string",
+        "start_": "string",
+        "end_": "string",
+        "day": "string",
+        "picture": "string",
+    }
+
+
 # End of Teri's test
 
 
+# Lakhvinder's test
 def test_get_restaurant_returns_404():
     # ARRANGE
     # Use our fake database
@@ -44,6 +68,9 @@ def test_get_restaurant_returns_404():
     # CLEAN UP
     # Clear out the dependencies
     app.dependency_overrides = {}
+
+
+# Lakhvinder's test
 
 
 # def test_get_category_returns_200():
