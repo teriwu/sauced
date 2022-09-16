34efa5ecb4ac0b102fe5aa4fc59c8fb39e1a2ac1
Teresa Wu <teresa.wu.jy@gmail.com>
2022-09-16 16:48:36 -0700

    Updated image alt name

--- ghi/src/LogIn.js
+++ ghi/src/LogIn.js
@@ -58,9 +58,9 @@ function LogIn() {
 
       <div className="col-md-9 col-lg-6 col-xl-5">
         <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-          className="img-fluid" alt="Sample image" />
+          className="img-fluid" alt="Log in" />
       </div>
-      
+
       <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
         <form onSubmit={handleSubmit}>
           <div>
